32c16c986a8061648db55ff78f502419fecc76ff
haruyama-makoto <makoto.haruyama@dena.com>
2015-08-17 01:13:22 +0900

    Update AWS SDK

--- lib/multi_storage/s3.rb
+++ lib/multi_storage/s3.rb
@@ -1,36 +1,47 @@
-require 'aws-sdk-core'
+require 'aws-sdk'
 require 'multi_storage/base'
 
 class MultiStorage
   class S3 < Base
+    # root_pathにはenvを入れて下さい
     def initialize(options = {})
       @root_path          = options[:s3_root]           || fail('require s3_root')
-      access_key_id       = options[:access_key_id]     || fail('require access_key_id')
-      secret_access_key   = options[:secret_access_key] || fail('require secret_access_key')
+      @access_key_id       = options[:access_key_id]     || fail('require access_key_id')
+      @secret_access_key   = options[:secret_access_key] || fail('require secret_access_key')
       @bucket             = options[:bucket]            || fail('require bucket_name')
-      @s3 = Aws::S3.new(
-        access_key_id:     access_key_id,
-        secret_access_key: secret_access_key
-      )
+      #Aws.config[:region] = options[:region]            || 'ap-northeast-1'
     end
 
     def get_content(stored_path)
       stored_s3_path = "#{@root_path}/#{stored_path}"
       # 存在しないkeyにアクセスするとAws::S3::Errors::NoSuchKeyで例外を吐く
       # あとで例外chatchしてnil返すようにする
-      entity = @s3.get_object(bucket: @bucket, key: stored_s3_path)
-      { type: :entity, body: entity.body.string }
+      s3 = AWS::S3.new(
+        access_key_id:     @access_key_id,
+        secret_access_key: @secret_access_key
+      )
+      url_string = s3.buckets[@bucket].objects[stored_s3_path].url_for(:read).to_s
+      #{ type: :entity, body: entity.read }
+      { type: :reproxy, body: url_string }
     end
 
     def create(stored_path, body)
       stored_s3_path = "#{@root_path}/#{stored_path}"
-      # over write
-      @s3.put_object(bucket: @bucket, key: stored_s3_path, body: body)
+      # 上書きする
+      s3 = AWS::S3.new(
+        access_key_id:     @access_key_id,
+        secret_access_key: @secret_access_key
+      )
+      s3.buckets[@bucket].objects.create(stored_s3_path, body)
     end
 
     def delete(stored_path)
       stored_s3_path = "#{@root_path}/#{stored_path}"
-      @s3.delete_object(bucket: @bucket, key: stored_s3_path)
+      s3 = AWS::S3.new(
+        access_key_id:     @access_key_id,
+        secret_access_key: @secret_access_key
+      )
+      s3.buckets[@bucket].objects[stored_s3_path].delete
     end
   end
 end
